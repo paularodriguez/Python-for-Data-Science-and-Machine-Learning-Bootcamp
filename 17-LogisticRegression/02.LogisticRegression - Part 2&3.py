1f106735e34b398eaddcfd7ad6029051e3c1dc5c
Paula <paula.rp93@gmail.com>
2020-03-29 14:04:42 +0200

    Logistic Regression: Part 3 - Create, train and predict a logistic regression

--- 17-LogisticRegression/02.LogisticRegression - Part 2&3.py
+++ 17-LogisticRegression/02.LogisticRegression - Part 2&3.py
@@ -135,3 +135,62 @@ print(train.head())
 # 2         1       3  26.0      0      0   7.9250     0  0  1
 # 3         1       1  35.0      1      0  53.1000     0  0  1
 # 4         0       3  35.0      0      0   8.0500     1  0  1
+separator()
+
+
+# STEP 3: Build and train the model
+
+print(train.columns)
+# Index(['Survived', 'Pclass', 'Age', 'SibSp', 'Parch', 'Fare', 'male', 'Q', 'S'], dtype='object')
+
+# features
+X = train[['Pclass', 'Age', 'SibSp', 'Parch', 'Fare', 'male', 'Q', 'S']]
+# column to predict
+y = train['Survived']
+separator()
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
+print(X_train.head())
+
+from sklearn.linear_model import LogisticRegression
+
+# Create and train the model (Logistic Regression)
+logmodel = LogisticRegression()
+logmodel.fit(X_train, y_train)
+
+predictions = logmodel.predict(X_test)
+
+
+# Evaluate the result
+from sklearn.metrics import classification_report
+
+print(classification_report(y_test, predictions))
+
+
+#               precision    recall  f1-score   support
+#
+#            0       0.82      0.92      0.87       163
+#            1       0.85      0.69      0.76       104
+#
+#     accuracy                           0.83       267
+#    macro avg       0.84      0.81      0.82       267
+# weighted avg       0.83      0.83      0.83       267
+
+# Confusion matrix
+
+from sklearn.metrics import confusion_matrix
+
+print(confusion_matrix(y_test, predictions))
+
+# [[150  13]
+#  [ 32  72]]
+
+# TT: 150
+# TN: 72
+# FP: 32
+# FN: 13
+# Accuracy: (150 + 72) / (150 + 13 + 32 + 72) = 0.83
+
+
